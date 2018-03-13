06c3e2633f6695fce4f6fe5c3f5e185048a6051b
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-03-13 16:41:00 -0400

    asdfsa

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = express.Router();
 app.set('views', 'views');  
   
 app.get('/', function (req, res) {
-  res.sendFile('/views/index.html');
+  res.sendFile('views/index.html');
 })
 
 app.listen(3000, function () {
