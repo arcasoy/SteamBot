4620fcc584c6d22b61abb4b3fc70a4fa43de3629
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-03-13 16:34:42 -0400

    asdfas

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-app.set('views', 'views'));  
+app.set('views', 'views');  
 app.use(‘/’,router);
   
 app.get('/', function (req, res) {
