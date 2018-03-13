7928f5685cd49a4e4be807a5c00aab19f43e18ca
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-03-13 14:22:00 -0400

    Showing index.ejs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index');
 })
 
 app.listen(3000, function () {
