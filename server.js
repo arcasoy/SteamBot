be3c9a6e57e8148d668e5c429d58f0681b4d637e
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-03-15 01:43:43 -0400

    sadf

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 
 app.use(express.static(__dirname + '/views/'));
+app.use(express.static(__dirname + '/scripts/'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
