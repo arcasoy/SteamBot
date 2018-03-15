d0c6fed678b3cf389b185976bdb3b13723e11728
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-03-15 09:41:55 -0400

    maybe??

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
 
