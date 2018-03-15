442521a1b635381fdeb1a43de11b80a04fe06389
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-03-15 09:27:09 -0400

    xzC

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ app.use(express.static(__dirname + '/scripts'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
-})
+});
 
 app.get('/', function (req, res) {
   res.sendFile('main.css');
-})
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
