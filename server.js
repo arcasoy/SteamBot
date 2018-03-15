5eb3ed2a394ad652887a0de7f418df35c1ea26e2
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-03-15 09:23:14 -0400

    z

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-app.use(express.static(__dirname + '/views/'));
-app.use(express.static(__dirname + '/scripts/'));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/scripts'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
+})
+
+app.get('/', function (req, res) {
+  res.sendFile('main.css');
 })
 
 app.listen(3000, function () {
