a2dbab018ae362a0fbdeef081dd43cbf3686b032
arcasoy <arcasoy1@gmail.com>
2018-03-20 14:12:26 -0400

    pdate

--- frontend/homepage.js
+++ frontend/homepage.js
@@ -3,18 +3,15 @@ function Input() {
 	var weaponValue = document.getElementById("weaponid").value;
 	var wearValue = document.getElementById("wearid").value;
 	console.log(weaponValue, wearValue);
+	var fso, f, r;
+ 	var ForReading = 1, ForWriting = 2;
+	fso = new ActiveXObject("Scripting.FileSystemObject");
+ 	f = fso.OpenTextFile("../backend/items.txt", ForWriting, true);
+ 	f.Write(weaponValue, wearValue);
+ 	f.Close();
+ 	f = fso.OpenTextFile("../backend/items.txt", ForReading);
+ 	r = f.ReadLine();
+ 	console.log(r);
+	console.log("Made to end");
 	return false;
 };
-
-/*function WriteDemo() 
-{
- var fso, f, r;
- var ForReading = 1, ForWriting = 2;
- fso = new ActiveXObject("Scripting.FileSystemObject");
- f = fso.OpenTextFile("c:\\testfile.txt", ForWriting, true);
- f.Write("Hello world!");
- f.Close();
- f = fso.OpenTextFile("c:\\testfile.txt", ForReading);
- r = f.ReadLine();
- return(r);
-}*/
