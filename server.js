3f293487274eeba25c612517fc6bfa58c0d4a8c8
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-04-05 16:30:03 -0400

    maybe now?

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var MongoClient = require('mongodb').MongoClient
 var mongourl = "mongodb://arcasoy1:Clo12!dog@cluster0-shard-00-00-ebxjm.mongodb.net:27017,cluster0-shard-00-01-ebxjm.mongodb.net:27017,cluster0-shard-00-02-ebxjm.mongodb.net:27017/SteamBot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
 MongoClient.connect(mongourl, function(err, db) {
+   db.close();
    if(!err) {
      console.log("Connected to MongoDB");
    }
