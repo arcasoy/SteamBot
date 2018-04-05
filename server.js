935ca4ba60b8aa42c70702e874ec2ab7f9a93b34
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-04-04 23:36:28 -0400

    Trying to connect to mongodb

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const mongo = require('mongodb');
 const app = express();
 const router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient
+var mongourl = "mongodb://Arcasoy:Clo12!dog@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+
+MongoClient.connect(mongourl, function(err, db) {
+   db.close();
+});
+
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
