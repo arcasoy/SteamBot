fc7b7ef5c22b5eb49f8c4e28d2c8f5847a5b3d4c
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-04-15 18:37:35 -0400

    zdfg

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ var MongoClient = require('mongodb').MongoClient
 var mongourl = "mongodb://arcasoy1:3141592653589793238462643383279@cluster0-shard-00-00-ebxjm.mongodb.net:27017,cluster0-shard-00-01-ebxjm.mongodb.net:27017,cluster0-shard-00-02-ebxjm.mongodb.net:27017/SteamBot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
 
 //request the list of CSGO skins from api here which will be used to test if/make a collection for each skin
-/*
-var allitems = (steam api for all items)
-*/
 request('http://api.steamapis.com/market/items/730?api_key=3euKunlWOMTCoRTjGXWEbDAmJ8c', function (error, response, json) {
   var allItems = JSON.parse(json);
-  console.log(allItems.data.market_name);
+  //console.log("allItems", allItems);
+  console.log(allItems.data[0].market_name);
 });
 //Connect to MongoDB and check connection. Then create collection for each skin if not already made
 MongoClient.connect(mongourl, function(err, db) {
