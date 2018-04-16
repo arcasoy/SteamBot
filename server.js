e941d98c109fe1f439a34254414c7c0a3123f59c
Alexander Arcasoy <32803978+arcasoy@users.noreply.github.com>
2018-04-16 00:56:39 -0400

    bm,

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient
-var mongourl = "mongodb://arcasoy1:3141592653589793238462643383279@cluster0-shard-00-00-ebxjm.mongodb.net:27017,cluster0-shard-00-01-ebxjm.mongodb.net:27017,cluster0-shard-00-02-ebxjm.mongodb.net:27017/SteamBot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+const mongourl = "mongodb://arcasoy1:3141592653589793238462643383279@cluster0-shard-00-00-ebxjm.mongodb.net:27017,cluster0-shard-00-01-ebxjm.mongodb.net:27017,cluster0-shard-00-02-ebxjm.mongodb.net:27017/SteamBot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+const dbName = 'SteamBot';
 
 //request the list of CSGO skins from api here which will be used to test if/make a collection for each skin
 request('http://api.steamapis.com/market/items/730?api_key=3euKunlWOMTCoRTjGXWEbDAmJ8c', function (error, response, json) {
@@ -13,7 +14,7 @@ request('http://api.steamapis.com/market/items/730?api_key=3euKunlWOMTCoRTjGXWEb
   console.log("There are", allItems.data.length, "CS:GO items on the market");
 
   //Connect to MongoDB and check connection. Then create collection for each skin if not already made
-  MongoClient.connect(mongourl, function(err, db) {
+  MongoClient.connect(mongourl, function(err, client) {
      if(!err) {
        console.log("Connected to MongoDB");
      }
@@ -21,9 +22,12 @@ request('http://api.steamapis.com/market/items/730?api_key=3euKunlWOMTCoRTjGXWEb
        console.log("Could not connect to MongoDB");
      }
 
+     const db = client.db(dbName);
+
      for (var i = 0; i < 1; i++) {
       var colName = allItems.data[i].market_name;
       console.log(colName);
+      //having troubles figuring out a way to find current collections and compare them to new ones being made
       db.collections(function(err, collections) {
         console.log(collections);
       });
